ee21fc3a00f2422bd0f12263aab475712c7f5248
Ethan142857 <144798245+Ethan142857@users.noreply.github.com>
2023-09-21 20:22:08 +0800

    Add files via upload

---
+++ title.py
@@ -0,0 +1,9 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> c = chr(0x2605)
+>>> print(f"{c}{c}{c}{c}{c}{c}{c}{c}{c}{c}{c}")
+★★★★★★★★★★★
+>>> print(f"{c}数据科学与工程导论{c}")
+★数据科学与工程导论★
+>>> print(f"{c}{c}{c}{c}{c}{c}{c}{c}{c}{c}{c}")
+★★★★★★★★★★★
